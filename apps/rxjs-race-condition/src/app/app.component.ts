855be46e7df571d54e9efad8cba67d9b4617d530
newmankyle <newmankyleSWL@gmail.com>
2023-02-21 17:01:56 -0800

    consider shareReplay solution

--- apps/rxjs-race-condition/src/app/app.component.ts
+++ apps/rxjs-race-condition/src/app/app.component.ts
@@ -1,11 +1,6 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { combineLatest, Observable, Subject, switchMap, take } from 'rxjs';
+import { Observable, shareReplay, take } from 'rxjs';
 import { TopicModalComponent } from './topic-dialog.component';
 import { TopicService, TopicType } from './topic.service';
 
@@ -21,26 +16,16 @@ export class AppComponent {
   topicService = inject(TopicService);
   topics$: Observable<TopicType[]> = this.topicService
     .fakeGetHttpTopic()
-    .pipe(take(1));
+    .pipe(take(1), shareReplay(1));
 
-  private topicClicker$ = new Subject<void>();
-
-  private handleDialog = this.topicClicker$
-    .pipe(
-      switchMap(() => this.topics$),
-      take(1)
-    )
-    .subscribe((topics) => {
+  openTopicModal() {
+    //I wonder if take(1) is also necessary here?
+    this.topics$.pipe(take(1)).subscribe((topics) => {
       this.dialog.open(TopicModalComponent, {
         data: {
           topics: topics,
         },
       });
     });
-
-  openTopicModal() {
-    this.topicClicker$.next();
   }
 }
-
-// faire une app qui fonctionne mais pas dans les tests e2e à cause du délai de la requete http.
